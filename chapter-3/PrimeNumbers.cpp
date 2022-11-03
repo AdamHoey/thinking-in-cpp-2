f124e492c974b531efff0aa615e9f3f18e5a4cfa
AdamHoey <103613405+AdamHoey@users.noreply.github.com>
2022-11-03 16:49:35 -0600

    Update PrimeNumbers.cpp
    Added text to introduce the program to the user and optimized the divisor test loop.

--- chapter-3/PrimeNumbers.cpp
+++ chapter-3/PrimeNumbers.cpp
@@ -8,20 +8,27 @@ by any other numbers except for themselves and 1).
 /* Version 1.0: Basic version. No error capture for invalid input */
 /* Version 1.1: Removed test for division by 1, since it is always the case.*/
 /* Version 1.2: Groups output values by lines of 10 items in a field length of 8.*/
+/* Version 1.3: Added introduction text and optimized testing loop*/
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     
     int userInt, divisorCount;
     int lineItem = 0; // holds number of items printed to a line
+    float maxDiv;  //  holds the highest divisor to test against a given integer
 
+    // Introduce the program and solicit user input.
+    cout << "This program will print out all of the prime numbers from 1 up to" << endl;
+    cout << "an integer of your choosing." << endl;
     cout << "Enter an integer: " << endl;
     cin >> userInt;
 
     for(int i = 1; i <= userInt; i++) {
         divisorCount = 0;
+        maxDiv = (int)sqrt(i);
         for(int d = 2; d <= i; d++) { // 1 is always a divisor and doesn't need to be tested
             if((i % d) == 0) divisorCount++;
         }
